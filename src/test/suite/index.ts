2e354d285031e19cfe73ee89272365e965c08f28
Jay Dhameliya <jdhameli@mathworks.com>
2023-03-16 19:17:19 -0400

    Add initial smoke tests
    - Added smoke tests for MATLAB - VS Code connection
    - Added VS Code Tester
    - Modified runTests.ts and index.ts to allow each test file to run in a fresh VS Code instance

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -1,38 +1,34 @@
+// Copyright 2023 The MathWorks, Inc.
+
 import * as path from 'path'
 import * as Mocha from 'mocha'
-import * as glob from 'glob'
 
-export function run (): Promise<void> {
+export async function run (): Promise<void> {
     // Create the mocha test
     const mocha = new Mocha({
         ui: 'tdd',
-        color: true
+        color: true,
+        timeout: 120000 // set default timeout to 2 minutes
     })
 
-    const testsRoot = path.resolve(__dirname, '..')
+    const testsRoot = path.resolve(__dirname)
 
-    return new Promise((resolve, reject) => {
-        glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
-            if (err) {
-                return reject(err)
-            }
+    return await new Promise((resolve, reject) => {
+        const test = process.env.test as string
+        mocha.addFile(path.resolve(testsRoot, test))
 
-            // Add files to the test suite
-            files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)))
-
-            try {
-                // Run the mocha test
-                mocha.run(failures => {
-                    if (failures > 0) {
-                        reject(new Error(`${failures} tests failed.`))
-                    } else {
-                        resolve()
-                    }
-                })
-            } catch (err) {
-                console.error(err)
-                reject(err)
-            }
-        })
+        try {
+            // Run the mocha test
+            mocha.run(failures => {
+                if (failures > 0) {
+                    reject(new Error(`${failures} tests failed.`))
+                } else {
+                    resolve()
+                }
+            })
+        } catch (err) {
+            console.error(err)
+            reject(err)
+        }
     })
 }
